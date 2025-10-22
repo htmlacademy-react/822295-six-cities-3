12b64ce6352d41c75bdad25e5f415d831cabd7a1
Daria Lapteva <d.pekhtereva@ndmsystems.com>
2025-10-22 15:03:20 +0300

    (chore) add clsx

--- src/components/locations/locations.tsx
+++ src/components/locations/locations.tsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
-const LOCATION_NAMES = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
+const locationNames = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'];
 
 
 function Locations(): JSX.Element {
   return (
     <section className="locations container">
       <ul className="locations__list tabs__list">
-        {LOCATION_NAMES.map((location) => (
+        {locationNames.map((location) => (
           <li className="locations__item" key={location}>
             <Link className="locations__item-link tabs__item" to={`/${location.toLowerCase()}`}>
               <span>{location}</span>
--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,9 +1,10 @@
+import clsx from 'clsx';
 import styles from './not-found-page.module.css';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div className="page page--gray">
-      <h1 className={`${styles.heading} ${styles['page--not-found']}`}>404. Page not found</h1>;
+    <div className={clsx('page', 'page--gray', styles['page--not-found'])}>
+      <h1 className={styles.heading}>404. Page not found</h1>;
     </div>
   );
 }
